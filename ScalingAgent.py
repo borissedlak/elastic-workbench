b3de9c2f2127c91986051b34e1d7ab27dc0b9328
boris PC <b.sedlak@dsg.tuwien.ac.at>
2024-11-14 14:18:57 +0100

    agent outline

--- ScalingAgent.py
+++ ScalingAgent.py
@@ -4,6 +4,15 @@ from threading import Thread
 from PrometheusClient import PrometheusClient
 
 
+# TODO: So what the agent must do on a high level is:
+#  1) Collect sensory state from Prometheus --> Easy
+#  2) Evaluate if SLOs are fulfilled --> Easy
+#  3) Retrain its interpretation model --> Difficult
+#  4) Act so that SLO-F is optimized --> Difficult
+#  _
+#  However, I assume that the agent has the variable DAG and the SLO thresholds
+#  And I dont have to resolve variable names dynamically, but keep them hardcoded
+
 class AIFAgent(Thread):
     def __init__(self):
         super().__init__()
