4f6db113d33c7d155bbf55e9d802589dc6bd77db
Boris Sedlak <boris.sedlak@dsg.tuwien.ac.at>
2025-05-15 16:23:51 +0200

    set up empty pymdp agent

--- iwai/pymdp_Agent.py
+++ iwai/pymdp_Agent.py
@@ -13,12 +13,14 @@ logger = logging.getLogger("multiscale")
 logger.setLevel(logging.DEBUG)
 
 
-class DAI_Agent(ScalingAgent):
+class pymdp_Agent(ScalingAgent):
     def __init__(self, prom_server, services_monitored: list[ServiceID], evaluation_cycle,
-                 slo_registry_path="../config/slo_config.json", es_registry_path="../config/es_registry.json",
-                 log_experience=None):
+                 slo_registry_path = "../config/slo_config.json", es_registry_path = "../config/es_registry.json",
+                 log_experience = None):
+
         super().__init__(prom_server, services_monitored, evaluation_cycle, slo_registry_path, es_registry_path,
                          log_experience)
+
 
     def get_optimal_local_ES(self, service: ServiceID, service_state, assigned_clients: Dict[str, int]):
         return None, {}
@@ -28,5 +30,5 @@ if __name__ == '__main__':
     ps = "http://localhost:9090"
     qr_local = ServiceID("172.20.0.5", ServiceType.QR, "elastic-workbench-qr-detector-1")
     cv_local = ServiceID("172.20.0.10", ServiceType.CV, "elastic-workbench-cv-analyzer-1")
-    DAI_Agent(services_monitored=[qr_local], prom_server=ps,
+    pymdp_Agent(services_monitored=[qr_local], prom_server=ps,
               evaluation_cycle=EVALUATION_CYCLE_DELAY).start()
