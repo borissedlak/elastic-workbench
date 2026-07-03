05d72174b3d79bd37537f9f99a8f53b5e2542176
borissedlak <bsedlak01@gmail.com>
2026-07-03 16:01:30 +0200

    first version of Katherine agent

---
+++ agent/Katherine_Agent.py
@@ -0,0 +1,19 @@
+from HttpClient import HttpClient
+from agent.components.es_registry import ESRegistry, ESType, ServiceID, ServiceType
+from agent.k8_Agent import SERVICE_HOST
+
+qr_local = ServiceID(SERVICE_HOST, ServiceType.QR, "elastic-workbench-qr-detector-1", port="8080")
+cv_local = ServiceID(SERVICE_HOST, ServiceType.CV, "elastic-workbench-cv-analyzer-1", port="8081")
+pc_local = ServiceID(SERVICE_HOST, ServiceType.PC, "elastic-workbench-pc-visualizer-1", port="8082")
+
+http_client = HttpClient()
+es_registry_path="../config/es_registry.json"
+es_registry = ESRegistry(es_registry_path)
+
+params = {'cores': 8} # TODO: Change parameters here (Look Readme)
+ES_endpoint = es_registry.get_es_information(qr_local.service_type, ESType.RESOURCE_SCALE)['endpoint'] # TODO: Change service and ESType here
+http_client.call_ES_endpoint(qr_local, ES_endpoint, params)
+
+params = {'data_quality': 350}
+ES_endpoint = es_registry.get_es_information(qr_local.service_type, ESType.QUALITY_SCALE)['endpoint']
+http_client.call_ES_endpoint(qr_local, ES_endpoint, params)
